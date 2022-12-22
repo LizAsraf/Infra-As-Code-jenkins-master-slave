2c07f61fd0750cda9b809ddbfe4c6ed8b611f985
Liz Asraf <liz161@gmail.com>
2022-12-22 23:52:30 +0200

    fixed oreder and dependencies again

--- modules/compute/jenkins.sh
+++ modules/compute/jenkins.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+workDir=/home/jenkins/agent
 docker start builder
 echo curl -sO http://${master}:8080/jnlpJars/agent.jar > slave.sh
 echo java -jar agent.jar -jnlpUrl http://${master}:8080/manage/computer/builder/jenkins-agent.jnlp -secret e1b175440b3c0148cac24af2bb0c617b2c23f68fc4ceb9310d79384b09b4ce8d -workDir ${workDir} >> slave.sh
